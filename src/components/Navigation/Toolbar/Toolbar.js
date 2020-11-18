9574561a860314684ca249004bac0d289864aad1
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-11-18 09:11:29 +0900

    [FEATURE](Toolbar) Add Toolbar

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Toolbar.module.css';
+
+const OrderSummary = (props) => (
+  <header className={styles.Toolbar}>
+    <div>MENU</div>
+    <div>LOGO</div>
+    <nav></nav>
+  </header>
+);
+
+export default OrderSummary;
