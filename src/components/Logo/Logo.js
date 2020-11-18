d9956c8792da8985ad5f9b28e85876271d029139
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-11-18 09:28:36 +0900

    [FEATURE](Toolbar) Add Logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import burgerLogo from '../../assets/images/burger-logo.png'
+
+import styles from './Logo.module.css';
+
+const Logo = () => (
+  <div className={styles.Logo}>
+    <img src={burgerLogo} alt="Burger logo" />
+  </div>
+);
+
+export default Logo;
