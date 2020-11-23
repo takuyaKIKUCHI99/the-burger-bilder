7ab7f6e4a6ab538201e97a273e1741e32f52a233
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-11-23 11:03:21 +0900

    [REFACTOR](Layout) Move to Container folder

---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,34 @@
+// React libraries
+import React, { useState } from 'react';
+import Proptypes from 'prop-types';
+
+// Style
+import styles from './Layout.module.css';
+
+// Components
+import Aux from '../../hoc/Aux';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+
+const Layout = ({ children }) => {
+  // State
+  const [isSideDrawerOpen, setIsSideDrawerOpen] = useState(false);
+
+  // Action handler
+  const openHandler = () => setIsSideDrawerOpen(true);
+  const closeHandler = () => setIsSideDrawerOpen(false);
+
+  return (
+    <Aux>
+      <Toolbar openHandler={openHandler} />
+      <SideDrawer isOpen={isSideDrawerOpen} closeHandler={closeHandler} />
+      <main className={styles.Content}>{children}</main>
+    </Aux>
+  );
+};
+
+Layout.propTypes = {
+  children: Proptypes.node.isRequired
+};
+
+export default Layout;
